62fd7fb87d102723de582d123e383729d9d3d950
Rohan Drape <rd@slavepianos.org>
2010-06-09 04:28:37 +0000

    add 'perform' function to nrt graph

--- src/non-real-time.hs
+++ src/non-real-time.hs
@@ -16,11 +16,23 @@ score =
         mk_instr = d_recv (synthdef "test" graph)
         mk_group = g_new [(1, AddToTail, 0)]
         mk_node t f = at t [s_new "test" (-1) AddToTail 1 [("freq", f)]]
-        notes = take 128 (zipWith mk_node [0.0, 0.05 ..] [330, 350 ..])
+        notes = take 128 (zipWith mk_node [1.0, 1.05 ..] [330, 350 ..])
     in at 0.0 [mk_instr, mk_group] : notes
+
+perform :: [OSC] -> IO ()
+perform s = do
+  let f i fd x = do let (Bundle (NTPr t) x') = x
+                    pauseThreadUntil (i + t)
+                    send fd (Bundle immediately x')
+  withSC3 (\fd -> utcr >>= \i -> mapM_ (f i fd) s)
+
+render :: [OSC] -> IO ()
+render s = do
+  writeNRT "/tmp/nrt.score" s
+  system "scsynth -N /tmp/nrt.score _ /tmp/nrt.wav 44100 WAVE float"
+  return ()
 
 main :: IO ()
 main = do
-  writeNRT "/tmp/nrt.score" score
-  system "scsynth -N /tmp/nrt.score _ /tmp/nrt.wav 44100 WAVE float"
-  return ()
+  render score
+  perform score
