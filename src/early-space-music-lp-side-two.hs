4b8bd2dcce4748f2b3dcf3ed6ae85f05d029e27c
Rohan Drape <rd@slavepianos.org>
2011-08-24 13:23:25 +0000

    early-space-music-lp-side-two

---
+++ src/early-space-music-lp-side-two.hs
@@ -0,0 +1,85 @@
+-- early space music LP, side 2 (jmcc)
+
+import Sound.OpenSoundControl
+import Sound.SC3.ID
+import qualified Sound.SC3.Lang.Collection.SequenceableCollection as C
+import Sound.SC3.Lang.Events.OverlapTexture
+import qualified System.Random as R
+
+m1 :: UGen
+m1 =
+    let a = rand 'a' 0 20
+	b = rand 'b' 0 5000
+	c = rand 'c' 0 20
+	p = rand 'd' (-1) 1
+    in pan2 (sinOsc AR (sinOsc AR a 0 * 0.1 * b + b) 0 * sinOsc KR c 0 * 0.08 + 0.08) p 1
+
+m2 :: UGen
+m2 =
+    let a0 = rand 'a' 40 240
+	a1 = a0 + (rand 'b' (-1) 1)
+	a = mce2 a0 a1
+	b = expRand 'c' 50 2400
+	c = mce2 (a0 + rand 'd' (-1) 1) (a1 + rand 'e' (-1) 1)
+    in sinOsc AR (sinOsc AR a 0 * rand 'f' 0 1 * b + b) 0 * sinOsc KR c 0 * 0.025 + 0.025
+
+m3 :: UGen
+m3 =
+    let f = midiCPS (rand 'a' 60 100)
+    in fSinOsc AR (mce2 f (f + 0.2)) 0 * lfNoise2 'b' KR (f * mce2 0.15 0.16) * 0.1
+
+m4 :: UGen
+m4 =
+    let f = midiCPS (rand 'a' 24 96)
+	r = xLine KR (expRand 'b' 0.1 20) (expRand 'c' 0.1 20) 25.6 DoNothing
+        a = lfPulse KR (expRand 'd' 0.2 1.2) 0 (rand 'e' 0.1 0.2)
+        o i = let i' = fromIntegral i
+                  e = max 0 (sinOsc KR (r * rand i 0.9 1.1) (rand i 0 (2 * pi)) * 0.1 - 0.05)
+                  s = fSinOsc AR (f * i' + f) 0 * e * (1 / (i' + 1))
+              in pan2 s (rand i (-1) 1) 1
+    in mixFill 12 o * a
+
+m6 :: UGen
+m6 =
+    let f = midiCPS (lfNoise1 'a' KR (rand 'b' 0 0.3) * 60 + 70)
+        a0 = lfNoise2 'c' AR (f * rand 'd' 0 0.5)
+        a1 = max 0 (lfNoise1 'e' KR (rand 'f' 0 8) * sinOsc KR (rand 'g' 0 40) 0 * 0.1)
+	z = sinOsc AR f 0 * a0 * a1
+    in pan2 z (lfNoise1 'h' KR (rand 'i' 0 5)) 1
+
+enumFromN :: Enum a => a -> Int -> [Int]
+enumFromN e i = let j = fromEnum e in [j .. j + i]
+
+m7 :: UGen
+m7 =
+    let p = 15
+        z = let y n = let fr = map (\e -> expRand e 100 6000) (enumFromN n p)
+                          rt = map (\e -> rand e 2 6) (enumFromN n p)
+                      in klankSpec fr (replicate p 1) rt
+            in mce2 (y 'a') (y 'A')
+        f = xLine KR (expRand 'α' 40 300) (expRand 'β' 40 300) 12 DoNothing
+        t = lfPulse AR f 0 (rand 'γ' 0.1 0.9) * 0.002 * max 0 (lfNoise2 'δ' KR (rand 'ε' 0 8))
+    in distort (klank t 1 0 1 (mceTranspose z)) * 0.1
+
+espmlps2 :: R.RandomGen g => g -> (UGen, g)
+espmlps2 g = C.choose' (map (* 0.3) [m1,m2,m3,m4,m6,m7]) g
+
+cmb :: Synthdef
+cmb =
+    let i = in' 2 AR 0
+        f e = combN i 0.3 (mce2 (rand e 0.1 0.3) (rand e 0.12 0.32)) 8
+    in synthdef "cmb" (replaceOut 0 (mixFill 5 f * 0.3))
+
+act :: Transport t => t -> IO ()
+act fd = do
+  _ <- async fd (d_recv cmb)
+  send fd (g_new [(2,AddToTail,0)])
+  send fd (s_new "cmb" (-1) AddToTail 2 [])
+  play fd (overlapTextureS' (2,4,6,maxBound) espmlps2 (R.mkStdGen 0))
+
+main :: IO ()
+main = withSC3 act
+
+{-
+audition (out 0 (m6 * 0.5))
+-}
