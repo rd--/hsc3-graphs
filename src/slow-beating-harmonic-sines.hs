fcffcac86ad16cb5112cb449dc83ca77252dc800
Rohan Drape <rd@slavepianos.org>
2011-08-24 09:45:13 +0000

    slow-beating[-harmonic-]sines.hs

---
+++ src/slow-beating-harmonic-sines.hs
@@ -0,0 +1,44 @@
+-- slow beating harmonic sines (jmcc)
+
+import Control.Monad
+import Control.Monad.Random
+import Sound.SC3
+import Sound.SC3.Lang.Events.OverlapTexture
+import Sound.SC3.Lang.Math.Random
+
+r_freq :: (RandomGen g) => Int -> Int -> Rand g [Double]
+r_freq k i = do
+  n <- nchoose i [0,2,4,5,7,9]
+  o <- nrand i 7
+  let f n' o' = midiCPS' (n' + (o' * 12) + fromIntegral k)
+  return (zipWith f n o)
+
+r_harmonics :: (RandomGen g) => Double -> Int -> Double -> Rand g [Double]
+r_harmonics d m f = do
+  d' <- nrand2 m d
+  return (zipWith (+) (map (* f) [1,2,4,5,6]) d')
+
+r_phase :: (RandomGen g) => Int -> Rand g [Double]
+r_phase m = nrrand m 0 (2 * pi)
+
+sbhs :: (RandomGen g) => Int -> Double -> Int -> Rand g UGen
+sbhs n d m = do
+  k' <- rand 12
+  let k = 24 + k'
+  f <- r_freq k n
+  p_fr <- mapM (r_harmonics d m) f
+  q_fr <- mapM (r_harmonics d m) f
+  p_ph <- replicateM n (r_phase m)
+  q_ph <- replicateM n (r_phase m)
+  let a = replicate n (replicate m 1)
+      p_sp = zipWith3 klangSpec' p_fr a p_ph
+      q_sp = zipWith3 klangSpec' q_fr a q_ph
+      mk_u s = klang AR 1 0 s * (0.1 / fromIntegral n)
+      p_u = sum (map mk_u p_sp)
+      q_u = sum (map mk_u q_sp)
+  return (mce2 p_u q_u)
+
+main :: IO ()
+main = do
+  g_ <- getStdGen
+  overlapTextureS (6,3,3,maxBound) (\g -> runRand (sbhs 8 0.4 5) g) g_
---
+++ src/slow-beating-sines.hs
@@ -0,0 +1,41 @@
+-- slow beating sines (jmcc)
+
+import Control.Monad
+import Control.Monad.Random
+import Sound.SC3
+import Sound.SC3.Lang.Events.OverlapTexture
+import Sound.SC3.Lang.Math.Random
+
+r_freq :: (RandomGen g) => Int -> Rand g [Double]
+r_freq i = do
+  n <- nrrand i 24 84
+  return (map midiCPS' n)
+
+r_harmonics :: (RandomGen g) => Double -> Int -> Double -> Rand g [Double]
+r_harmonics d m f = nrand2 m d >>= return . map (+ f)
+
+r_harmonics' :: (RandomGen g) => Double -> Int -> Double -> Rand g [Double]
+r_harmonics' d m f = nrand2 (m - 1) d >>= return . (f :)  . map (+ f)
+
+r_phase :: (RandomGen g) => Int -> Rand g [Double]
+r_phase m = nrrand m 0 (2 * pi)
+
+sbs :: (RandomGen g) => Int -> Double -> Int -> Rand g UGen
+sbs n d m = do
+  f <- r_freq n
+  p_fr <- mapM (r_harmonics' d m) f
+  q_fr <- mapM (r_harmonics d m) f
+  p_ph <- replicateM n (r_phase m)
+  q_ph <- replicateM n (r_phase m)
+  let a = replicate n (replicate m 1)
+      p_sp = zipWith3 klangSpec' p_fr a p_ph
+      q_sp = zipWith3 klangSpec' q_fr a q_ph
+      mk_u s = klang AR 1 0 s * (0.1 / fromIntegral n)
+      p_u = sum (map mk_u p_sp)
+      q_u = sum (map mk_u q_sp)
+  return (mce2 p_u q_u)
+
+main :: IO ()
+main = do
+  g_ <- getStdGen
+  overlapTextureS (4,4,3,maxBound) (\g -> runRand (sbs 20 0.4 3) g) g_
