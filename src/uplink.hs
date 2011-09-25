80e32396c23130bbcc969bdf552f7f3098df147e
Rohan Drape <rd@slavepianos.org>
2011-09-25 00:38:24 +0000

    hsc3-graphs/scritto -> Data/Vowel

--- src/uplink.hs
+++ src/uplink.hs
@@ -9,12 +9,14 @@ randz e n = rand e 0 n
 uplink :: UGen
 uplink =
     let r = randz
-        p0 = lfPulse KR (r 'a' 20) 0 (r 'b' 1)
-	p1 = lfPulse KR (r 'c' 4) 0 (r 'd' 1) * r 'e' 8000 + r 'f' 2000
-        p2 = lfPulse KR (r 'g' 20) 0 (r 'h' 1)
-        p3 = lfPulse KR (r 'i' 4) 0 (r 'j' 1) * r 'k' 8000 + r 'l'  2000
-        f = p0 * p1 + p2 * p3
-    in pan2 (lfPulse AR f 0 0.5 * 0.04) (rand 'm' (-0.8) 0.8) 1
+        p0 = lfPulse KR (r 'α' 20) 0 (r 'α' 1)
+	p1 = lfPulse KR (r 'β' 4) 0 (r 'β' 1) * r 'β' 8000 + r 'β' 2000
+        f = mix (uclone 'γ' 2 (p0 * p1))
+    in pan2 (lfPulse AR f 0 0.5 * 0.04) (rand 'δ' (-0.8) 0.8) 1
 
 main :: IO ()
 main = overlapTextureU (4,1,5,maxBound) uplink
+
+{-
+Sound.SC3.UGen.Dot.draw uplink
+-}
