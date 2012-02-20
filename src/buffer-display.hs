acf86b1ef1792bc9eb4765e2997248a2215cad1d
Rohan Drape <rd@slavepianos.org>
2012-02-20 00:08:43 +0000

    buffer-display (shell)

--- src/buffer-display.hs
+++ src/buffer-display.hs
@@ -1,6 +1,7 @@
 -- http://sccode.org/1-1HR (f0)
 -- partial...
 
+import Data.CG.Minus {- hcg-minus -}
 import Data.IORef
 import qualified Foreign.C.Math.Double as M {- cmath -}
 import qualified Graphics.Rendering.Cairo as C {- cairo -}
@@ -9,20 +10,23 @@ import qualified Graphics.UI.Gtk.Gdk.Events as E
 import Sound.OpenSoundControl {- hosc -}
 import qualified Sound.SC3 as S {- hsc3 -}
 
-type R = Double
+-- * Type
+
 data BD_Type = BD_Line | BD_Warp | BD_Flower
-data BD = BD {sc3 :: UDP
-             ,theta :: R
-             ,buffer :: Int
+data BD = BD {buffer :: Int
              ,buffer_sz :: Int
+             ,buffer_data :: [R]
              ,cnt :: Int
-             ,trails :: R
-             ,speed :: R
              ,amplitude :: R
+             ,theta :: R
+             ,theta_incr :: R
+             ,trails :: R
              ,version :: BD_Type}
 
-default_bd :: UDP ->  BD
-default_bd fd = BD fd 0 10 512 0 1 0 0.5 BD_Line
+default_bd :: BD
+default_bd = BD 10 512 [] 0 0.5 0 0 1 BD_Line
+
+-- * Render
 
 rotate_about :: R -> R -> R -> C.Render ()
 rotate_about a x y = do
@@ -30,106 +34,132 @@ rotate_about a x y = do
   C.rotate a
   C.translate (-x) (-y)
 
-pt_rotate :: R -> (R,R) -> (R,R)
-pt_rotate a (x,y) =
-    let s = sin a
-        c = cos a
-    in ((x * c) - (y * s),(y * c) + (x * s))
-
-render_line :: BD -> [R] -> R -> C.Render ()
-render_line bd d n = do
-  let a = amplitude bd
-  rotate_about (theta bd) (n/2) (n/2)
+render_line :: R -> R -> [R] -> R -> C.Render ()
+render_line a t d n = do
+  rotate_about t (n/2) (n/2)
   C.translate 0 (n/2)
   let f (x,y) = let y' = (y * n * a)
                 in if x == 0 then C.moveTo x y' else C.lineTo x y'
   mapM_ f (zip [0..] d)
   C.stroke
 
-render_warp :: BD -> [R] -> R -> C.Render ()
-render_warp bd d n = do
-  let a = amplitude bd
-  rotate_about (theta bd) (n/2) (n/2)
+pt_rotate' :: Floating a => a -> a -> a -> (a,a)
+pt_rotate' a x y = pt_xy (pt_rotate a (pt x y))
+
+render_warp :: R -> R -> [R] -> R -> C.Render ()
+render_warp a t d n = do
+  rotate_about t (n/2) (n/2)
   C.translate (n/2) (n/2)
-  let f (x,y) = let (x',y') = pt_rotate (y * 2 * pi) (x * a,y * a)
+  let f (x,y) = let (x',y') = pt_rotate' (y * 2 * pi) (x * a) (y * a)
                 in if x == 0 then C.moveTo x' y' else C.lineTo x' y'
   mapM_ f (zip [0..] d)
   C.stroke
 
-render_flower :: BD -> [R] -> R -> C.Render ()
-render_flower bd d n = do
+render_flower :: R -> [R] -> R -> C.Render ()
+render_flower t d n = do
   let d0 = d !! 0
   C.translate (n/2) (n/2)
   C.moveTo (d0 * d0) 0
-  let f (x,y) = let a = ((x `M.fmod` n) / n) * 2 * pi + theta bd
-                    (x',y') = pt_rotate a (y * y,x * y)
+  let f (x,y) = let a = ((x `M.fmod` n) / n) * 2 * pi + t
+                    (x',y') = pt_rotate' a (y * y) (x * y)
                 in C.lineTo x' y'
   mapM_ f (zip [0..] d)
   C.stroke
 
-render_bd :: BD -> [R] -> C.Render BD
-render_bd bd d = do
+bd_render :: RENDER_F BD
+bd_render bd = do
   let n = fromIntegral (buffer_sz bd)
+      d = buffer_data bd
+      a = amplitude bd
+      t = theta bd
   C.rectangle 0 0 n n
   C.setSourceRGBA 0 0 0 (trails bd)
   C.fill
   C.setSourceRGBA 0 1 0 1
   case version bd of
-    BD_Line -> render_line bd d n
-    BD_Warp -> render_warp bd d n
-    BD_Flower -> render_flower bd d n
+    BD_Line -> render_line a t d n
+    BD_Warp -> render_warp a t d n
+    BD_Flower -> render_flower t d n
+  return (bd {theta = theta bd + theta_incr bd
+             ,cnt = cnt bd + 1})
+
+-- * SC3
+
+bd_init :: INIT_F BD
+bd_init fd bd = do
+  _ <- S.async fd (S.b_alloc (buffer bd) (buffer_sz bd) 1)
   return bd
 
-unf :: Datum -> R
-unf d =
-    case d of
-      Float x -> x
-      Double x -> x
-      _  -> error "unf: non-float"
-
-read_b :: BD -> IO [R]
-read_b bd = do
+bd_sc3 :: SC3_F BD
+bd_sc3 fd bd = do
   let b = buffer bd
-      fd = sc3 bd
       n = buffer_sz bd
   S.send fd (S.b_getn b [(0,n)])
   r <- wait fd "/b_setn"
-  case r of
-    Message "/b_setn" (_:Int 0:Int _:xs) -> return (map unf xs)
-    _ -> return []
+  let d = case r of
+            Message "/b_setn" (_:Int 0:Int _:xs) -> map datum_real' xs
+            _ -> []
+  return (bd {buffer_data = d})
 
-update :: G.DrawingArea -> C.Surface -> IORef BD -> IO Bool
-update c s r = do
-  w <- G.widgetGetDrawWindow c
-  bd <- readIORef r
-  d <- read_b bd
-  _ <- C.renderWith s (render_bd bd d)
-  C.surfaceFlush s
-  _ <- G.renderWithDrawable w (C.setSourceSurface s 0 0 >> C.paint)
-  writeIORef r (bd {theta = theta bd + speed bd
-                   ,cnt = cnt bd + 1})
-  return True
+-- * Key
 
-keypress :: G.WidgetClass w => w -> IORef BD -> E.Event -> IO Bool
-keypress w r e = do
-  let nm = E.eventKeyName e
+bd_key :: KEY_F BD
+bd_key k bd =
+    case k of
+      'T' -> bd {trails = trails bd + 0.025}
+      't' -> bd {trails = trails bd - 0.025}
+      'S' -> bd {theta_incr = theta_incr bd + 0.025}
+      's' -> bd {theta_incr = theta_incr bd - 0.025}
+      'w' -> bd {version = BD_Warp}
+      'l' -> bd {version = BD_Line}
+      'f' -> bd {version = BD_Flower}
+      'z' -> bd {theta = 0}
+      _ -> bd
+
+-- * Shell
+
+bd_shell :: SHELL BD
+bd_shell = (default_bd,bd_init,bd_sc3,bd_render,bd_key)
+
+-- * Shell Implemetation
+
+type RENDER_F st = st -> C.Render st
+type INIT_F st = UDP -> st -> IO st
+type SC3_F st = UDP -> st -> IO st
+type KEY_F st = Char -> st -> st
+type SHELL st = (st,INIT_F st,SC3_F st,RENDER_F st,KEY_F st)
+
+sh_keypress_f :: G.WidgetClass w => w -> IORef st -> KEY_F st -> E.Event -> IO Bool
+sh_keypress_f w r key_f e = do
+  let kv = E.eventKeyVal e
+      nm = E.eventKeyName e
   case nm of
     "Escape" -> G.widgetDestroy w
-    "T" -> modifyIORef r (\bd -> bd {trails = trails bd + 0.1})
-    "t" -> modifyIORef r (\bd -> bd {trails = trails bd - 0.1})
-    "S" -> modifyIORef r (\bd -> bd {speed = speed bd + 0.1})
-    "s" -> modifyIORef r (\bd -> bd {speed = speed bd - 0.1})
-    "w" -> modifyIORef r (\bd -> bd {version = BD_Warp})
-    "l" -> modifyIORef r (\bd -> bd {version = BD_Line})
-    "f" -> modifyIORef r (\bd -> bd {version = BD_Flower})
-    _ -> print nm
+    _ -> case G.keyToChar kv of
+           Just c -> modifyIORef r (key_f c)
+           _ -> return ()
   return True
 
-bd_run :: BD -> IO (IORef BD)
-bd_run bd = do
-  r <- newIORef bd
-  let n = buffer_sz bd
-      n' = fromIntegral n
+sh_update_f :: G.WidgetClass w =>
+               w -> C.Surface -> IORef st -> UDP -> SC3_F st -> RENDER_F st -> IO Bool
+sh_update_f c s r fd sc3_f render_f = do
+  w <- G.widgetGetDrawWindow c
+  bd <- readIORef r >>= sc3_f fd >>= C.renderWith s . render_f
+  C.surfaceFlush s
+  _ <- G.renderWithDrawable w (C.setSourceSurface s 0 0 >> C.paint)
+  writeIORef r bd
+  return True
+
+sc3_fd :: IO UDP
+sc3_fd = openUDP "127.0.0.1" 57110
+
+shell :: Int -> SHELL st -> IO ()
+shell n sh = do
+  let n' = fromIntegral n
+      (i_st,init_f,sc3_f,render_f,key_f) = sh
+  fd <- sc3_fd
+  st <- init_f fd i_st
+  r <- newIORef st
   s <- C.createImageSurface C.FormatARGB32 n n
   C.renderWith s (do C.rectangle 0 0 n' n'
                      C.setSourceRGBA 0 0 0 1
@@ -137,23 +167,20 @@ bd_run bd = do
   _ <- G.initGUI
   w <- G.windowNew
   c <- G.drawingAreaNew
-  _ <- S.async (sc3 bd) (S.b_alloc (buffer bd) n 1)
   G.windowSetResizable w False
   G.widgetSetSizeRequest w n n
-  _ <- G.onKeyPress w (keypress w r)
+  _ <- G.onKeyPress w (sh_keypress_f w r key_f)
   _ <- G.onDestroy w G.mainQuit
-  _ <- G.onExpose c (const (update c s r))
+  _ <- G.onExpose c (const (sh_update_f c s r fd sc3_f render_f))
   _ <- G.timeoutAdd (G.widgetQueueDraw w >> return True) 15
   G.set w [G.containerChild G.:= c]
   G.widgetShowAll w
   G.mainGUI
-  return r
-
-sc3_fd :: IO UDP
-sc3_fd = openUDP "127.0.0.1" 57110
 
 main :: IO ()
-main = fmap default_bd sc3_fd >>= bd_run >> return ()
+main = do
+  let n = buffer_sz default_bd
+  shell n bd_shell
 
 {-
 import Sound.SC3
@@ -170,8 +197,5 @@ let {v = varSaw AR (mouseX KR 1 1000 Linear 0.2) 0 0.5
     ;o = out 0 s}
 in audition (mrg [o,r])
 
-fd <- sc3_fd
-bd_run (BD fd 0 10 512 0 0.1 0 0.5 BD_Line)
-bd_run (BD fd 0 10 512 0 1 0 0.5 BD_Warp)
-bd_run (BD fd 0 10 512 0 1 0 0.5 BD_Flower)
+shell 512 bd_shell
 -}
