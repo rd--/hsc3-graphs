d80a485e6bbcf6092282001eec132c9e49f592bb
Rohan Drape <rd@slavepianos.org>
2012-04-20 00:30:07 +0000

    +f0-2012-04-19

---
+++ src/f0-2012-04-19.hs
@@ -0,0 +1,13 @@
+-- https://www.listarc.bham.ac.uk/lists/sc-users/msg17536.html
+import Sound.SC3
+
+f0 :: UGen
+f0 = let s0 = lfSaw AR 10 0 * 0.01
+         t0 = lfTri AR (mce2 5 6 * 0.1) 0
+         t1 = lfTri KR 0.1 0 * 0.05 + 0.05
+         s1 = limiter (brf s0 t0 1) 1 0.01
+         o = combN s1 0.1 (roundTo t1 0.01) 1
+     in out 0 (o * 0.1)
+
+main :: IO ()
+main = audition f0
