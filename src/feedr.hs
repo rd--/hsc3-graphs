efd3140c3a049fba73c19dc6018e9ffb5d24fbd5
Rohan Drape <rd@slavepianos.org>
2012-07-15 06:04:00 +0000

    .lhs->.hs

---
+++ src/feedr.hs
@@ -0,0 +1,36 @@
+-- feedr (rd)
+-- warning: input/output feedback loop
+
+import Sound.OpenSoundControl {- hosc -}
+import Sound.SC3.Monadic {- hsc3 -}
+
+delayWr :: UGen -> UGen -> UGen
+delayWr b = recordBuf AR b 0 1 0 1 Loop 0 DoNothing
+
+tap :: Int -> UGen -> UGen -> UGen
+tap nc b dt = playBuf nc AR b 1 0 (dt * (- sampleRate)) Loop DoNothing
+
+type State = (UGen,Double)
+
+feedr :: (UId m) => State -> Int -> m UGen
+feedr (ch,dl) n = do
+  t <- sequence (replicate n (rand 0.0 (constant dl)))
+  g <- sequence (replicate n (rand 0.4 1.0))
+  f <- sequence (replicate n (rand 0.9 0.95))
+  let d = zipWith (\p q -> tap 2 10 p * q) t g
+      x = mouseX KR 0.02 1.0 Exponential 0.1
+      s = clip2 (leakDC (hpf (sum d) 20) 0.995) 1
+      i = soundIn ch
+      r = i + sum (map (* x) (zipWith (*) d f))
+  return (mrg [out 0 s, delayWr 10 r])
+
+run :: Transport t => State -> t -> IO ()
+run (ch,dl) fd = do
+  nf <- fmap (* dl) (serverSampleRateActual fd)
+  send fd (b_alloc 10 (floor nf) 2)
+  audition =<< feedr (ch,dl) 18
+
+main :: IO ()
+main = withSC3 (run (mce2 4 5,6))
+
+-- withSC3 (\fd -> send fd (b_zero 10))
---
+++ src/rm-octaver.hs
@@ -0,0 +1,15 @@
+-- rm-octaver (andrea valle, miller puckette)
+
+import Sound.SC3
+
+defaultPitch :: UGen -> UGen
+defaultPitch x = pitch x 440 60 4000 100 16 1 0.01 0.5 1
+
+rm_octaver :: UGen -> UGen
+rm_octaver i =
+    let p = defaultPitch i
+        f = mceChannel 0 p
+    in sinOsc AR (f * 0.5) 0 * i + i
+
+main :: IO ()
+main = audition (out 0 (rm_octaver (soundIn 4)))
