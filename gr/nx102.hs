2b2770106cf21140ead756eba41fa7039f4c39bc
Rohan Drape <rd@slavepianos.org>
2012-08-25 03:56:49 +0000

    ~mk:nx102

--- gr/nx102.hs
+++ gr/nx102.hs
@@ -1,6 +1,6 @@
 -- http://sccode.org/1-4QO
 
-import Sound.SC3.ID {-hsc3 -}
+import Sound.SC3.ID {- hsc3 -}
 import Sound.SC3.Lang.Control.OverlapTexture {- hsc3-lang -}
 
 -- > Sound.SC3.UGen.Dot.draw nx102
