3eb729d496b6ac42c055ecc8d2741a2bb8d4cc75
Rohan Drape <rd@slavepianos.org>
2013-04-23 07:47:00 +0000

    +dot/fm-iter

--- gr/fm-iter.hs
+++ gr/fm-iter.hs
@@ -1,15 +1,18 @@
 -- fm-iter (rd)
 
 import Sound.SC3.ID {- hsc3 -}
+import Sound.SC3.Lang.Control.OverlapTexture {- hsc3-lang -}
 
+-- audition (out 0 fm_iter)
 fm_iter :: UGen
 fm_iter =
-  let t0 = impulse AR (recip 0.30) 0
+  let f0 = recip (0.3 + whiteNoise 'α' AR * 0.01)
+      t0 = impulse AR f0 0
       t1 = tDelay t0 0.15
       t = mce2 t0 t1
-      k = tRand 'α' 56 57 t
-      i = tRand 'β' 40 480 t
-      j = tRand 'γ' (-1) 1 t
+      k = tRand 'β' 56 57 t
+      i = tRand 'γ' 40 480 t
+      j = tRand 'δ' (-1) 1 t
       c = midiCPS k
       m = midiCPS (k + 1 + j)
       s = envPerc 0.01 0.9
@@ -18,4 +21,4 @@ fm_iter =
   in sinOsc AR f 0 * e
 
 main :: IO ()
-main = audition (out 0 fm_iter)
+main = overlapTextureU (3,3,6,maxBound) fm_iter
