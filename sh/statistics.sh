34bb9d9bfb6ef5e51ecbea00211ec604c2a531c2
Rohan Drape <rd@slavepianos.org>
2014-06-08 07:47:04 +0000

    ~readme/statistics

--- sh/statistics.sh
+++ sh/statistics.sh
@@ -1,8 +1,9 @@
-grep \(adc\) */*.hs
-grep \(am\) */*.hs
-grep \(f0\) */*.hs | wc -l
-grep \(jl\) */*.hs
-grep \(jmcc\) */*.hs | wc -l
-grep \(jrhb\) */*.hs
-grep \(nc\) */*.hs
-grep \(nv\) */*.hs
+echo adc; grep \(adc\) */*.hs | wc -l
+echo am; grep \(am\) */*.hs | wc -l
+echo f0; grep \(f0\) */*.hs | wc -l
+echo jl; grep \(jl\) */*.hs | wc -l
+echo jmcc; grep \(jmcc\) */*.hs | wc -l
+echo jrhb; grep \(jrhb\) */*.hs | wc -l
+echo nc; grep \(nc\) */*.hs | wc -l
+echo nv; grep \(nv\) */*.hs | wc -l
+echo rd; grep \(rd\) */*.hs | wc -l
