387ada4c50aace6bb27469a149958f053f87efc9
Rohan Drape <rd@slavepianos.org>
2014-06-08 14:03:45 +0000

    +gr/plucked-strings.scd

--- gr/plucked-strings-m.hs
+++ gr/plucked-strings-m.hs
@@ -11,9 +11,9 @@ plucked_strings = do
              r1 <- rand 0.05 0.15
              r2 <- rand 0 (pi * 2)
              r3 <- iRand 0 2
-             let s0 = impulse AR r0 0.3
+             let s0 = impulse AR r0 0 * 0.3
                  s1 = n0 * 0.3
-                 s2 = impulse AR (sinOsc KR r1 r2 * 5 + 5.2) 0.3
+                 s2 = impulse AR (sinOsc KR r1 r2 * 5 + 5.2) 0 * 0.3
              return (select r3 (mce [s0,s1,s2]))
       s = do n0 <- pinkNoise AR
              r1 <- rand (-1) 1
