69103a58fcb2ce5650f0362b64f2c8aa4c515660
Rohan Drape <rd@slavepianos.org>
2014-07-28 10:02:33 +0000

    +gr/lots-o-sines.fs

--- gr/coolant.fs
+++ gr/coolant.fs
@@ -1,8 +1,9 @@
 \ coolant (jmcc) #2
 
-: param 10 0 do 0 2000 Rand.ir 40 + 1 1 loop 30 mce ;
-: n BrownNoise.ar 2 clone 0.002 * 0.95 OnePole ;
-: coolant n 1 0 1 param Klank.ar 2 clone ;
+: n 20 ;
+: param n 0 do 0 2000 Rand.ir 40 + 1 1 loop n 3 * mce ;
+: src BrownNoise.ar 2 clone 0.0015 * 0.95 OnePole ;
+: coolant src 1 0 1 param Klank.ar 2 clone ;
 : coolants coolant 4 4 2 inf overlap-texture ;
 
 coolants
