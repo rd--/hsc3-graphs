ae4f18ddb2def76dc80dc24baa75e27162642377
Rohan Drape <rd@slavepianos.org>
2014-07-28 12:37:00 +0000

    ~gr/strummable-guitar.fs

--- gr/strummable-guitar.fs
+++ gr/strummable-guitar.fs
@@ -2,7 +2,7 @@
 
 : scale 52 57 62 67 71 76 ;
 
-: str { ix sc }
+: str { sc ix }
     0 1 linear 0.2 MouseX.kr { x }
     x 0.25 ix 0.1 * + > HPZ1.kr abs { t }
     t 0.05 Decay.kr PinkNoise.ar * { n }
@@ -10,6 +10,6 @@
     n dt dt 4 CombL.ar { s }
     s ix 0.2 * 0.5 - 1 Pan2.ar ;
 
-: strs 0 52 str 1 57 str + 2 62 str + 3 67 str + 4 71 str + 5 76 str + ;
+: strs 52 0 str 57 1 str + 62 2 str + 67 3 str + 71 4 str + 76 5 str + ;
 
 strs play
