799cfb1685795ff8125305e5b09c20481d39112a
Rohan Drape <rd@slavepianos.org>
2014-08-27 13:13:27 +0000

    +clustered-sines.fs,rail.fs

---
+++ gr/clustered-sines.fs
@@ -0,0 +1,11 @@
+\ clustered sines (jmcc) #2
+
+: cs
+    80 { n }
+    100 1100 Rand.ir { f1 }
+    4 f1 *  { f2 }
+    n 0 do 0 f2 Rand.ir f1 + { y } y f1 y / 0 loop n 3 * mce { param }
+    1 0 param Klang.ar 0.3 n / * 2 clone
+;
+
+cs 4 4 inf xfade-texture
