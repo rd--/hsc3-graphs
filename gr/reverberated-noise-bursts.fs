44cfe1349e95a3bcbcea663c0e5e11922e2c3028
Rohan Drape <rd@slavepianos.org>
2014-08-28 14:20:49 +0000

    +gr/metal-plate,+gr/reverberated-noise-bursts.fs

---
+++ gr/reverberated-noise-bursts.fs
@@ -0,0 +1,7 @@
+\ reverberated noise bursts (jmcc) #3
+
+: s 0.6 dust.ar 0.2 * 0.15 decay pinknoise.ar * 0.6 dust.ar 0.2 * 0.15 decay pinknoise.ar * ;
+: z 0.048 0.048 delayn ;
+: y 0.1 0 0.1 rand.ir 6 clone lfnoise1.kr 0.04 * 0.05 + 15 combl mix ;
+: f 4 0 do 0.05 0 0.05 rand.ir 2 clone 1 allpassn loop ;
+s y f + play
