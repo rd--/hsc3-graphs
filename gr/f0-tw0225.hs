236832b9c1f3c00b741ee6040d8f77bd2b079d9a
Rohan Drape <rd@slavepianos.org>
2014-10-10 00:50:16 +0000

    +gr/f0-tw0225

---
+++ gr/f0-tw0225.hs
@@ -0,0 +1,12 @@
+-- http://www.fredrikolofsson.com/f0blog/?q=node/617
+
+import Sound.SC3 {- hsc3 -}
+
+f0_tw0225 :: UGen
+f0_tw0225 =
+    let b = mce [1..8] * 99
+        o = blip AR (b / 2 + lfSaw KR ((-8) / b) 1 * 99) (b / 4 + (lfSaw KR (1 / b) 1 * 99))
+    in sin (splay (combN (o * sinOsc AR (8 / b) (lfSaw AR (99 / b) 0)) 0.2 0.2 1) 1 1 0 True)
+
+main :: IO ()
+main = audition (out 0 f0_tw0225)
--- gr/synthetic-piano.fs
+++ gr/synthetic-piano.fs
@@ -1,9 +1,8 @@
 \ synthetic piano (jmcc) #3
 
-: vib 1 LFNoise1.kr 3 7 muladd 0.02 0 0 0.04 0.1 0 Vibrato.kr ;
 : c { o f ph n }
     3000 LFNoise2.ar f ph Impulse.ar
-    0.1 * 0.008 0.04 Decay2 * 1 n o + MIDICPS vib / dup
+    0.1 * 0.008 0.04 Decay2 * 1 n o + MIDICPS / dup
     6 CombL
 ;
 : sp { n }
@@ -12,5 +11,4 @@
     -0.05 0 0.04 0 3 0 do swap f ph n c + loop n 36 - 27 / 1 - 1 Pan2
 ;
 : sp-r 36 90 IRand.ir sp ;
-\ sp-r 18 0 6 inf overlap-texture
-sp-r 6 clone mix play
+sp-r 18 0 6 inf overlap-texture
