eef3babd12b8d2b9d57907a4f6eb91bdf36f6e9a
Rohan Drape <rd@slavepianos.org>
2014-10-14 07:13:52 +0000

    +gr/f0-feedback

---
+++ gr/f0-feedback.hs
@@ -0,0 +1,37 @@
+-- http://www.fredrikolofsson.com/f0blog/?q=node/619 (f0)
+
+import Sound.OSC {- hsc3 -}
+import Sound.SC3 {- hsc3 -}
+
+feedback4 :: UGen
+feedback4 =
+    let k = control KR
+        freq = k "freq" 400
+        amp = k "amp" 0.1
+        fb = k "fb" 2
+        del = k "del" 0.1
+        lfo = k "lfo" 1
+        rate = k "rate" 2
+        cutoff = k "cutoff" 500
+        env = envGen KR 1 1 0 1 RemoveSynth (envPerc 1  4)
+        lin = delayN (hpf (localIn 1 AR 0) cutoff) 1 del
+        src = sinOsc AR (freq + sinOsc AR rate 0 * lfo) (lin * 2 * pi * fb) * amp * env
+    in mrg2 (out 0 (pan2 src 0 1)) (localOut src)
+
+type Param = [(String, Double)]
+
+fb :: Param -> IO ()
+fb p = audition_at (-1,AddToHead,1,p) feedback4 >> pauseThread 2
+
+param :: [Param]
+param =
+    [[]
+    ,[("del",0.5)]
+    ,[("del",0.1),("cutoff",100)]
+    ,[("del",0.1),("fb",3),("cutoff",100)]
+    ,[("del",0.1),("fb",2),("lfo",15),("cutoff",100)]
+    ,[("del",0.2),("fb",3),("lfo",100),("freq",100),("cutoff",100)]
+    ,[("del",0.5),("fb",1),("lfo",200),("freq",300),("rate",0.01),("cutoff",100)]]
+
+main :: IO ()
+main = mapM_ fb param
