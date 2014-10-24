0948bd3bfef005355004017f1b463d7b358618d6
Rohan Drape <rd@slavepianos.org>
2014-10-24 22:39:25 +0000

    +gr/pond-life-m

--- gr/pond-life-hp.hs
+++ gr/pond-life-hp.hs
@@ -6,11 +6,10 @@ import Sound.SC3.Lang.Control.OverlapTexture {- hsc3-lang -}
 
 pond_life :: UId m => m UGen
 pond_life = do
-  let f = fSinOsc KR (20 + #(randM 0 30)) 0
-          * #(randM 100 400)
-          + #(linRandM 500 2500 0)
+  let f0 = 20 + #(randM 0 30)
+  let f1 = fSinOsc KR f0 0 * #(randM 100 400) + #(linRandM 500 2500 0)
   let a = lfPulse KR (3 / #(randM 1 9)) 0 #(randM 0.2 0.5) * 0.04
-  return (pan2 (sinOsc AR f 0 * a) #(randM (-1) 1) 0.5)
+  return (pan2 (sinOsc AR f1 0 * a) #(randM (-1) 1) 0.5)
 
 main :: IO ()
 main = overlapTextureU (8,8,4,maxBound) =<< pond_life
