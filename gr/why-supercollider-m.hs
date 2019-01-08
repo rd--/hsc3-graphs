0b4329b3b2af2e78a6834f0bab466a6058c8c86d
rd <rd@slavepianos.org>
2019-01-08 22:51:32 +0000

    ~gr/why-supercollider-m.hs,+fix

--- gr/why-supercollider-m.hs
+++ gr/why-supercollider-m.hs
@@ -3,12 +3,12 @@ import Sound.SC3.Common.Base {- hsc3 -}
 import Sound.SC3.UGen.M {- hsc3-m -}
 
 -- > draw why_supercollider
-why_supercollider :: UId m => UGen m
+why_supercollider :: UGen UId_ST
 why_supercollider =
     let r = resonz (dust AR 0.2 * 50) (rand 200 3200) 0.003
-        s = sum_opt (replicate 10 r)
+        s = fix (sum_opt (replicate 10 r))
         c = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 KR (rand 0 0.1) * 0.04 + 0.05) 15
-        y = sum_opt (replicate 7 c)
+        y = fix (sum_opt (replicate 7 c))
         f i = allpassN i 0.05 (mce2 (rand 0 0.05) (rand 0 0.05)) 1
         x = compose_l (replicate 4 f) y
     in s + 0.2 * x
