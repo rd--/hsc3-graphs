29f6cfc306287faab4ea1f5ea746303733113a8a
rd <rd@slavepianos.org>
2019-01-08 23:46:50 +0000

    +why-sc3/m

---
+++ Sound/SC3/Graphs/JMcC/Why_SuperCollider/Monadic.hs
@@ -0,0 +1,28 @@
+-- | why supercollider (jmcc) #0
+module Sound.SC3.Graphs.JMcC.Why_SuperCollider.Monadic where
+
+import Control.Monad {- base -}
+
+import Sound.SC3 {- hsc3 -}
+
+-- > audition =<< why_supercollider_m
+-- > Sound.SC3.UGen.Dot.draw =<< why_supercollider_m
+-- > putStrLn . synthstat =<< why_supercollider_m
+why_supercollider_m :: UId m => m UGen
+why_supercollider_m = do
+  let r = do
+        n1 <- dustM AR 0.2
+        n2 <- randM 200 3200
+        return (resonz (n1 * 50) n2 0.003)
+  s <- fmap sum_opt (replicateM 10 r)
+  let c = do
+        n1 <- randM 0 0.1
+        n2 <- lfNoise1M KR n1
+        return (combL (delayN s 0.048 0.048) 0.1 (n2 * 0.04 + 0.05) 15)
+  y <- fmap sum_opt (replicateM 7 c)
+  let f i = do
+        n1 <- randM 0 0.05
+        n2 <- randM 0 0.05
+        return (allpassN i 0.05 (mce2 n1 n2) 1)
+  x <- chainM 4 f y
+  return (s + 0.2 * x)
