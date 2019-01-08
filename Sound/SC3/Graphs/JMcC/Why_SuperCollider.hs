065b2eadc1c685cda9ac7e496e656fb27db4427b
rd <rd@slavepianos.org>
2019-01-08 22:41:43 +0000

    ~why-sc3

--- Sound/SC3/Graphs/JMcC/Why_SuperCollider.hs
+++ Sound/SC3/Graphs/JMcC/Why_SuperCollider.hs
@@ -11,9 +11,9 @@ import qualified Sound.SC3.UGen.Bindings.DB.RDU as RDU {- sc3-rdu -}
 why_supercollider_plain :: UGen
 why_supercollider_plain =
     let r z = resonz (dust z AR 0.2 * 50) (rand z 200 3200) 0.003
-        s = sum (map r (id_seq 10 'α'))
+        s = sum_opt (map r (id_seq 10 'α'))
         c z = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 z KR (rand z 0 0.1) * 0.04 + 0.05) 15
-        y = sum (map c (id_seq 7 'β'))
+        y = sum_opt (map c (id_seq 7 'β'))
         f z i = allpassN i 0.05 (RDU.randN 2 z 0 0.05) 1
         x = Base.compose_l (map f (id_seq 4 'γ')) y
     in s + 0.2 * x
--- gr/why-supercollider-m.hs
+++ gr/why-supercollider-m.hs
@@ -1,13 +1,14 @@
 import Sound.SC3.Common {- hsc3 -}
+import Sound.SC3.Common.Base {- hsc3 -}
 import Sound.SC3.UGen.M {- hsc3-m -}
 
 -- > draw why_supercollider
 why_supercollider :: UId m => UGen m
 why_supercollider =
     let r = resonz (dust AR 0.2 * 50) (rand 200 3200) 0.003
-        s = sum (replicate 10 r)
+        s = sum_opt (replicate 10 r)
         c = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 KR (rand 0 0.1) * 0.04 + 0.05) 15
-        y = sum (replicate 7 c)
+        y = sum_opt (replicate 7 c)
         f i = allpassN i 0.05 (mce2 (rand 0 0.05) (rand 0 0.05)) 1
         x = compose_l (replicate 4 f) y
     in s + 0.2 * x
