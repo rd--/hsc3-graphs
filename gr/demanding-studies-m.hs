6858abb39d118a865db787ca24ca3b4ea758084c
rd <rd@slavepianos.org>
2019-01-10 03:29:25 +0000

    +gr/demanding-studies-m.hs

---
+++ gr/demanding-studies-m.hs
@@ -0,0 +1,19 @@
+import Sound.SC3.Common {- hsc3 -}
+import Sound.SC3.UGen.M {- hsc3-m -}
+
+-- > draw demanding_studies
+demanding_studies :: UId m => UGen m
+demanding_studies =
+  let s1 = drand dinf (mce [72, 75, 79, 82])
+      s2 = drand 1 (mce [82, 84, 86])
+      s3 = dseq dinf (mce [72, 75, 79, s2])
+      x = mouseX KR 5 13 Linear 0.2
+      tr = impulse KR x 0
+      f = demand tr 0 (mce2 (midiCPS (s1 - 12)) (midiCPS s3))
+      o1 = sinOsc AR (f + mce2 0 0.7) 0
+      o2 = saw AR (f + mce2 0 0.7) * 0.3
+      o3 = cubed (distort (log (distort (o1 + o2))))
+  in o3 * 0.1
+
+main :: IO ()
+main = audition demanding_studies
