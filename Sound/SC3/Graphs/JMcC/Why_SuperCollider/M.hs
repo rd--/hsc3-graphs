c618bacbf03e7901ce2f6f287d8a3660717afcd5
rd <rd@slavepianos.org>
2019-01-14 05:44:53 +0000

    fix->share,compose_l->foldl

--- Sound/SC3/Graphs/JMcC/Why_SuperCollider/M.hs
+++ Sound/SC3/Graphs/JMcC/Why_SuperCollider/M.hs
@@ -1,17 +1,17 @@
 -- | why supercollider (jmcc) #0
 module Sound.SC3.Graphs.JMcC.Why_SuperCollider.M where
 
+import Data.Function {- base -}
 import Sound.SC3.Common {- hsc3 -}
-import Sound.SC3.Common.Base {- hsc3 -}
 import Sound.SC3.UGen.M {- hsc3-m -}
 
 -- > synthstat_wr why_supercollider
 why_supercollider :: UGen
 why_supercollider =
     let r = resonz (dust AR 0.2 * 50) (rand 200 3200) 0.003
-        s = fix (mix (dup 10 r))
+        s = share (mix (dup 10 r))
         c = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 KR (rand 0 0.1) * 0.04 + 0.05) 15
-        y = fix (mix (dup 7 c))
+        y = share (mix (dup 7 c))
         f i = allpassN i 0.05 (mce2 (rand 0 0.05) (rand 0 0.05)) 1
-        x = compose_l (replicate 4 f) y
+        x = foldl (&) y (replicate 4 f)
     in s + 0.2 * x
--- Sound/SC3/Graphs/JMcC/Why_SuperCollider/Plain.hs
+++ Sound/SC3/Graphs/JMcC/Why_SuperCollider/Plain.hs
@@ -1,8 +1,8 @@
 -- | why supercollider (jmcc) #0
 module Sound.SC3.Graphs.JMcC.Why_SuperCollider.Plain where
 
+import Data.Function {- base -}
 import Sound.SC3 {- hsc3 -}
-import qualified Sound.SC3.Common.Base as Base {- hsc3 -}
 
 -- > synthstat_wr why_supercollider
 why_supercollider :: UGen
@@ -12,5 +12,5 @@ why_supercollider =
       c z = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 z KR (rand z 0 0.1) * 0.04 + 0.05) 15
       y = sum_opt (map c (id_seq 7 'β'))
       f z i = allpassN i 0.05 (mce2 (rand (z,'γ') 0 0.05) (rand (z,'δ') 0 0.05)) 1
-      x = Base.compose_l (map f (id_seq 4 'ε')) y
+      x = foldl (&) y (map f (id_seq 4 'ε'))
   in s + 0.2 * x
