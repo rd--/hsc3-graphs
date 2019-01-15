0863107ccf49807a4d6b30bba0308cafa5b85006
rd <rd@slavepianos.org>
2019-01-15 23:57:37 +0000

    ~hsc3,mce_gen/cpy

--- Sound/SC3/Graphs/JMcC/Why_SuperCollider/Monadic.hs
+++ Sound/SC3/Graphs/JMcC/Why_SuperCollider/Monadic.hs
@@ -1,8 +1,6 @@
 -- | why supercollider (jmcc) #0
 module Sound.SC3.Graphs.JMcC.Why_SuperCollider.Monadic where
 
-import Control.Monad {- base -}
-
 import Sound.SC3 {- hsc3 -}
 
 why_supercollider_m :: UId m => m UGen
@@ -11,12 +9,12 @@ why_supercollider_m = do
         n1 <- dustM AR 0.2
         n2 <- randM 200 3200
         return (resonz (n1 * 50) n2 0.003)
-  s <- fmap sum_opt (replicateM 10 r)
+  s <- fmap mix (mce_genM r 10)
   let c = do
         n1 <- randM 0 0.1
         n2 <- lfNoise1M KR n1
         return (combL (delayN s 0.048 0.048) 0.1 (n2 * 0.04 + 0.05) 15)
-  y <- fmap sum_opt (replicateM 7 c)
+  y <- fmap mix (mce_genM c 7)
   let f i = do
         n1 <- randM 0 0.05
         n2 <- randM 0 0.05
