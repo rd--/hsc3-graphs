d019b3b52ad6cae062049ebc2f5f1f39b07184f8
rohan <rd@slavepianos.org>
2022-02-20 08:17:37 +1100

    +lib/scd/graph/dcv-time-varying-feedback-delay-network.scd

--- js/polyglot.js
+++ js/polyglot.js
@@ -5,7 +5,7 @@ var current_hash;
 function polyglot_load_syndef_and_play() {
     var fileName = 'db/' + current_hash + '.scsyndef';
     console.log('load_syndef_and_play: filename', current_hash);
-    load_array_buffer_and_then(fileName, syndefData => playSyndef(current_hash, syndefData));
+    load_arraybuffer_and_then(fileName, syndefData => playSyndef(current_hash, syndefData));
 }
 
 function polyglot_init() {
