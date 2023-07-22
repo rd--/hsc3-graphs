f0525dc297f3b2002de6ad2cbbb07b73a650c52d
rohan <rd@slavepianos.org>
2023-07-22 10:43:05 +1000

    ~jssc3

--- js/polyglot.js
+++ js/polyglot.js
@@ -5,7 +5,7 @@ let current_hash;
 export function load_syndef_and_play() {
     const fileName = `db/${current_hash}.scsyndef`;
     console.log(`load_syndef_and_play: ${fileName}`);
-    sc.load_arraybuffer_and_then(fileName, function(syndefData) {
+    sc.fetch_arraybuffer_then(fileName, function(syndefData) {
 		sc.scSynthEnsure(globalScSynth, function() {
 				 sc.playSynDef(globalScSynth, current_hash, syndefData, 1);
 		});
@@ -13,7 +13,7 @@ export function load_syndef_and_play() {
 }
 
 export function initialise() {
-    sc.load_json_and_then("json/db.json", function(db) {
+    sc.fetch_json_then("json/db.json", function(db) {
         const select = document.getElementById('polyglotList');
         db.forEach(e => sc.select_add_option_to(select, e.hash + e.format, e.header));
     });
@@ -21,7 +21,7 @@ export function initialise() {
 		console.log(`initialise: ${value}`);
         current_hash = value.split('.')[0];
         console.log(current_hash);
-        sc.load_utf8_and_then('db/' + value, function(text) {
+        sc.fetch_utf8_then('db/' + value, function(text) {
             document.getElementById('polyglotText').value = text;
         });
     });
