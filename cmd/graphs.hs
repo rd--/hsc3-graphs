aa1b9a33dd02d457f086c77e177877560f48c0b4
rohan <rd@slavepianos.org>
2024-03-02 17:01:28 +1100

    +f0-tw-1762790022623674534

--- cmd/graphs.hs
+++ cmd/graphs.hs
@@ -9,7 +9,7 @@ import Sound.Sc3.Graphs.Polyglot {- hsc3-graphs -}
 
 >>> fn <- Music.Theory.Directory.dir_subset graphs_db_fext graphs_db_dir
 >>> length fn
-6063
+6064
 -}
 graphs_db_dir :: FilePath
 graphs_db_dir = "/home/rohan/sw/hsc3-graphs/db/"
